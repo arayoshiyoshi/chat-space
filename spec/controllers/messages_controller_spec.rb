1b2af3095b9eb75df16063b81384859b709cf0ae
arayoshiyoshi <60563213+arayoshiyoshi@users.noreply.github.com>
2020-03-24 19:10:52 +0900

    rspec実装

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,92 @@
+require 'rails_helper'
+
+describe MessagesController do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+
+    context 'log in' do
+      before do
+        login user
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'assigns @message' do
+        expect(assigns(:message)).to be_a_new(Post)
+      end
+
+      it 'assigns @group' do
+        expect(assigns(:group)).to eq group
+      end
+
+      it 'renders index' do
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :index, params: { group_id: group.id }
+      end
+
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    let(:post_params) { { group_id: group.id, user_id: user.id, post: attributes_for(:post) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: post_params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Post, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, post: attributes_for(:post, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Post, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: post_params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+end
+
