0093684f56ceca36ead6046bcc4aab8b8136368e
arayoshiyoshi <60563213+arayoshiyoshi@users.noreply.github.com>
2020-03-27 17:12:27 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,38 @@
 $(function(){
-  function buildHTML(message){
-    if(message.image){
-      // 画像があるとき
-      var html =
-        `<div class='message'>
-          <div class='message__info'>
-            <div class='message__info--name'>
-              ${message.user_name}
-            </div>
-            <div class='message__info--date-time'>
-              ${message.created_at}
-            </div>
-          </div>
-          <div class='message__text'>
-            <p class='message__text--plaintext'>
-              ${message.content}
-            </p>
-            <p>
-              <img class="message__text--image" src="${message.image}" alt="Test image" />
-            </p>
-          </div>
-        </div>`
-    }else{
-      // 画像がないとき
-      var html = 
-        `<div class='message'>
-          <div class='message__info'>
-            <div class='message__info--name'>
-              ${message.user_name}
-            </div>
-            <div class='message__info--date-time'>
-              ${message.created_at}
-            </div>
-          </div>
-          <div class='message__text'>
-            <p class='message__text--plaintext'></p>
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-    }
+
+  var buildHTML = function(message){
+    var html =
+      `<div class="message" data-message-id=` + message.id + `>
+      <div class="message__info">
+        <div class="message__info--name">` +
+          message.user_name +
+        `</div>
+        <div class="message__info--date-time">` +
+          message.created_at +
+        `</div>
+      </div>
+      <div class="message__text">
+      `
+
+    if (message.content && message.image) {
+      var html = html + 
+        `<p class="message__text--plaintext">` +
+          message.content +
+        `</p>
+        <img src="` + message.image + `" class="message__text--image" >
+        `
+    } else if (message.content) {
+      var html = html + 
+        `<p class="message__text--plaintext">` +
+          message.content +
+        `</p>
+        `
+    } else if (message.image) {
+      var html = html +
+        `<img src="` + message.image + `" class="message__text--image" >`
+    };
+    var html = html + 
+      `</div>`
     return html;
   }
 
@@ -63,4 +56,30 @@ $(function(){
       $('.submit-btn').prop('disabled', false);
     })
   })
+
+  var reloadMessages = function(){
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  }
 });
